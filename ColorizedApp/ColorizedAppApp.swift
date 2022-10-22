48c6919bf0f3716c6461ee5a4112886ead0598ae
Samir <imnotajedi@icloud.com>
2022-10-22 20:47:26 +0600

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Самир Кафаров on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
